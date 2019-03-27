13273074d9d206932f2bb3b2663ec6537c0d4d9b
Timothy Egenton <tegenton@ucvts.tec.nj.us>
2019-03-27 14:44:16 -0400

    input

--- src/Game.java
+++ src/Game.java
@@ -13,8 +13,10 @@ public class Game {
     private Deck deck;
     private ArrayList<Card> hand;
     private ArrayList<Card> dealer;
+    private static Scanner in;
 
     Game(int balance, games game) {
+        in = new Scanner(System.in);
         deck = new Deck();
         this.balance = balance;
         switch (game) {
@@ -32,15 +34,22 @@ public class Game {
 
     private void blackJack() {
         deck = new Deck();
-        while (balance > 0) {
+        loop: while (balance > 0) {
             System.out.println("Current Balance: $" + this.balance);
             System.out.println("Would you like to play a hand? It will cost $2");
             if (balance < 2) {
                 System.out.println("You cannot afford it!");
-                break;
+                break loop;
             }
             else {
-                // TODO: user input
+                String temp = "";
+                while (true) {
+                    temp = (String) getInput(String.class);
+                    if (temp.equals("yes"))
+                        break;
+                    else if (temp.equals("no"))
+                        break loop;
+                }
                 startBlackJack();
             }
         }
@@ -51,6 +60,13 @@ public class Game {
     }
 
     private void startBlackJack() {
+        balance -= 2;
+        int bet;
+
+        do {
+            System.out.println("Starting bet is $2, how much more would you like to add?");
+        } while ((bet = (int) getInput(Integer.class) + 2) < 2);
+
         boolean cont = true;
         while (cont) {
             System.out.print("Current hand: ");
@@ -64,6 +80,7 @@ public class Game {
             hit(this.dealer, 2);
             printHand(this.dealer, true);
             System.out.println(" = " + sum(this.dealer, true));
+
         }
     }
 
@@ -115,6 +132,32 @@ public class Game {
         }
         for (int i = (dealer) ? 1 : 0; i < whichHand.size(); i++) {
             System.out.print(whichHand.get(i).getCardRank() + ((i != whichHand.size() - 1) ? ", " : ""));
+        }
+    }
+
+    private Object getInput(Class<?> type) {
+        String temp;
+        while (true) {
+            try {
+                temp = in.nextLine();
+                if (type.isAssignableFrom(String.class)) {
+                    return type.cast(temp);
+                }
+                else if (type.isAssignableFrom(Integer.class)) {
+                    return Integer.parseInt(temp);
+                }
+                else if (type.isAssignableFrom(Boolean.class)) {
+                    return Boolean.parseBoolean(temp);
+                }
+                else if (type.isAssignableFrom(Double.class)) {
+                    return Double.parseDouble(temp);
+                }
+                else {
+                    throw new IllegalArgumentException("Bad type.");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid Input");
+            }
         }
     }
 }
