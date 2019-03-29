2cd5957b68901581433aae49b9fca1b920823bd9
Timothy Egenton <tegenton@ucvts.tec.nj.us>
2019-03-29 14:05:16 -0400

    finish blackjack

--- src/Game.java
+++ src/Game.java
@@ -5,8 +5,6 @@ public class Game {
 
     enum games {
         BLACKJACK,
-        POKER,
-        TEXAS
     }
 
     private int balance;
@@ -22,12 +20,6 @@ public class Game {
         switch (game) {
             case BLACKJACK:
                 blackJack();
-                break;
-            case POKER:
-                poker();
-                break;
-            case TEXAS:
-                texas();
                 break;
         }
     }
@@ -50,6 +42,11 @@ public class Game {
                     else if (temp.equals("no"))
                         break loop;
                 }
+
+                hand = new ArrayList<>();
+                hit(this.hand, 2);
+                dealer = new ArrayList<>();
+                hit(this.dealer, 2);
                 startBlackJack();
             }
         }
@@ -60,36 +57,83 @@ public class Game {
     }
 
     private void startBlackJack() {
-        balance -= 2;
         int bet;
-
         do {
+            bet = 2;
             System.out.println("Starting bet is $2, how much more would you like to add?");
-        } while ((bet = (int) getInput(Integer.class) + 2) < 2);
+            bet += (int) getInput(Integer.class);
+        } while ((bet < 2) || bet > balance);
+        balance -= bet;
 
         boolean cont = true;
         while (cont) {
+            System.out.println("You have bet $" + bet);
             System.out.print("Current hand: ");
-            hand = new ArrayList<>();
-            hit(this.hand, 2);
             printHand(this.hand, false);
             System.out.println(" = " + sum(this.hand, false));
 
             System.out.print("Dealer's hand: ");
-            dealer = new ArrayList<>();
-            hit(this.dealer, 2);
             printHand(this.dealer, true);
             System.out.println(" = " + sum(this.dealer, true));
 
+            int input;
+            do {
+                System.out.println("What would you like to do?\n 1. Hit\n 2. Stand\n 3. Double Down");
+                input = (int) getInput(Integer.class);
+            } while (input < 1 || input > 3);
+
+            switch (input) {
+                case 1:
+                    hit(this.hand, 1);
+                    break;
+                case 2:
+                    cont = false;
+                    break;
+                case 3:
+                    do {
+                        System.out.println("How much more would you like to bet? you can add up to $" + bet);
+                        input = (int) getInput(Integer.class);
+                    } while (input > bet);
+                    this.balance -= input;
+                    hit(this.hand, 1);
+                    cont = false;
+                    break;
+                default:
+                    break;
+            }
+            if (sum(this.hand, false) > 21)
+                cont = false;
         }
-    }
 
-    private void poker() { // TODO
-        System.out.println("Not yet implemented");
-    }
+        int i = 0;
+        while (sum(this.dealer, false) < 17) {
+            hit(this.dealer, 1);
+            i++;
+        }
+        System.out.println("Dealer takes " + i + " cards.");
 
-    private void texas() { // TODO
-        System.out.println("Not yet implemented");
+        System.out.print("Current hand: ");
+        printHand(this.hand, false);
+        System.out.println(" = " + sum(this.hand, false));
+
+        System.out.print("Dealer's hand: ");
+        printHand(this.dealer, false);
+        System.out.println(" = " + sum(this.dealer, false));
+
+        if (sum(this.hand, false) > 21) {
+            System.out.println("Dealer wins!");
+        }
+        else if (sum(this.dealer, false) > 21) {
+            System.out.println("You won!");
+            this.balance += 2 * bet;
+        }
+        else if (sum(this.hand, false) > sum(this.dealer, false)) {
+            System.out.println("You won!");
+            this.balance += 2 * bet;
+        }
+        else {
+            System.out.println("Dealer won!");
+        }
     }
 
     private void hit(ArrayList<Card> whichHand, int i) {
@@ -139,7 +183,7 @@ public class Game {
         String temp;
         while (true) {
             try {
-                temp = in.nextLine();
+                temp = in.nextLine().toLowerCase();
                 if (type.isAssignableFrom(String.class)) {
                     return type.cast(temp);
                 }
